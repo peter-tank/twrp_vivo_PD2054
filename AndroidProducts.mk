1efbc884278f3ea9628838baf7cf858a4cc641e9
peter-tank <30540412+peter-tank@users.noreply.github.com>
2023-10-28 16:00:00 +0000

    Initial commit
    Signed-off-by: peter-tank <30540412+peter-tank@users.noreply.github.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_PD2054.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_PD2054-user \
+    omni_PD2054-userdebug \
+    omni_PD2054-eng
